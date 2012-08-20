03d8cfe2c3616bc158d03a15d7c14e97212aa90e
chepecarlos <chepecarlos@alswblog.org>
2012-08-20 11:17:39 -0600

    Simplificando la funcion Disco Disco y agregando la frecuencia para hacer retardos

--- Aura_LilyPad_Open_Heart.ino
+++ Aura_LilyPad_Open_Heart.ino
@@ -29,6 +29,7 @@ char Pin[19] = {
   A5,A4,A3,A2,A1,A0,12,13,11,10,9,8,7,6,5,4,3,2,1};//Posiciones de led
 char nivel[19];
 int Aura = 7;
+int Frecuencia = 1;
 
 void setup() {
 
@@ -40,58 +41,41 @@ void setup() {
 
   Limpiar();
 
-  }//Funcion de inicializacion de variables
+}//Funcion de inicializacion de variables
 
-  void loop() {
-    E15_DiscoDisco();
-  }//Funcion que se repitira en el sistema
+void loop() {
+  E15_DiscoDisco();
+}//Funcion que se repitira en el sistema
 
 void Limpiar(){
   for(int Led = 0; Led < TotalLed; Led++)
     nivel[Led] = 0;
+  Frecuencia = 1;
 }//Funcion que pone todos los led apagados
 
 void Actualizar(){
- // nivel[0]=20;
-  for(int t = 0; t < 100; t++){
+  for(int t = 0; t < 10*Frecuencia; t++){
     for(int Led = 0; Led < TotalLed; Led++){
-      if(nivel[Led] <=  t)
+      if(nivel[Led]*Frecuencia <=  t)
         digitalWrite(Pin[Led], LOW);
       else
-        digitalWrite(Pin[Led], HIGH);
-     /* if( t % 2 == 0){
-      digitalWrite(Pin[Led], LOW);  
-      }*/
-      
+        digitalWrite(Pin[Led], HIGH);     
     }
     delay(1);
   }
 }//Enciende los led dependidendo del nivel que estes
 
 void E15_DiscoDisco(){
-  int j = 0;
-  int v = -1;
-  int i;
-  float t0 = millis();
-  float t1 = t0;
   Limpiar();
+  Frecuencia = 10;
+  for(int Led = 0; Led < TotalLed; Led++){
+    nivel[Led] = 6;
+  }
   do{
-    t1 = millis();
-
-    if( t1 - t0 >= 200){
-      t0 = millis();
-      for(i = 0; i < TotalLed; i++){
-        nivel[i] = j;
-      }
-
-      if( j == -1 || j == 102)//102
-        v = -v;
-      j = j + v;  
-    }
-
     Actualizar();
-
   }
   while(digitalRead(0) == 1);
 }//El primer esta un pwm de todos los LED
+
+
 
